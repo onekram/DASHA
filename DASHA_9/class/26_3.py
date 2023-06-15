421265a2f8c13287268e2238f1dc598a9f1d21ae
Ivan Kramarenko <ivankramarenko13@gmail.com>
2023-06-15 16:57:40 +0300

    changes

--- DASHA_9/class/26_3.py
+++
@@ -1,7 +0,0 @@
-with open('26_117.txt') as f:
-    k, n = [int(i) for i in f.readline().split()]
-    a = [tuple(map(int, line.split())) for line in f.readlines()]
-d = [0] * 10_000
-for item in a:
-    for i in range(item[0], item[1] + 30 + 1):
-        d[i] += 1
