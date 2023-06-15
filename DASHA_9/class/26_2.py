4d87594287e8f311a511a4e24a2852ffe1feed83
Ivan Kramarenko <ivankramarenko13@gmail.com>
2023-06-15 11:10:47 +0300

    made 26 1257

--- DASHA_9/class/26_2.py
+++ DASHA_9/class/26_2.py
@@ -1,12 +1,14 @@
+# задание 26 1257 из курса
 from itertools import combinations
 
 with open('26_1257.txt') as f:
     n = int(f.readline())
     a = [int(f.readline()) for i in range(n)]
+    b = set(a)  # поиск в множестве происходит быстрее чем в списке, поэтому задача решается быстро достаточно
 m = []
-print(a)
+# use set b to find sums
 for x, y in combinations(a, 2):
-    if (x % 2 != y % 2) and (x + y) in a:
+    if (x % 2 != y % 2) and (x + y) in b:
         m.append(x + y)
 
 print(max(m), len(m))
